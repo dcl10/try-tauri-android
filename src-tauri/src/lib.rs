63315ae1d818dffafc714d64cdedbdcaae96510f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-10-02 14:41:39 +0100

    primes logic

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,43 @@
+use std::usize;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str, primes: usize) -> String {
+    let first_n_primes = find_primes(primes);
+    let first_n_primes_str = first_n_primes
+        .into_iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join(", ");
     format!(
-        "Hello, {}! You've been greeted from Rust! The first {} primes are: ",
-        name, primes
+        "Hello, {}! You've been greeted from Rust! The first {} primes are: {}.",
+        name, primes, first_n_primes_str
     )
+}
+
+fn find_primes(n: usize) -> Vec<usize> {
+    let mut primes = Vec::new();
+
+    for value in 0..usize::MAX {
+        let mut add_value = false;
+        if value == 2 {
+            primes.push(value);
+        }
+        for prev in primes.iter() {
+            add_value = false;
+            if value % prev == 0 {
+                break;
+            }
+            add_value = true;
+        }
+        if add_value {
+            primes.push(value)
+        }
+        if primes.len() == n {
+            break;
+        }
+    }
+    primes
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
