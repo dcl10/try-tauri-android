616f61121de30c75c622cbb7b79aa21975f3ae40
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-10-02 13:25:05 +0100

    first commit

---
+++ src-tauri/gen/android/app/src/main/java/com/daniel/try_tauri_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.daniel.try_tauri_android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
